81c07b3825542c155f553068592853906de5efef
keigo <ketchup1216@icloud.com>
2021-11-15 14:59:15 +0900

    account layout

--- app/src/main/java/com/websarva/wings/android/wispace/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/wispace/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.websarva.wings.android.wispace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun login(view: View){
+
+    }
+
+    fun register(view: View){
+        val intent = Intent(this@MainActivity, Register::class.java)
+        startActivity(intent)
     }
 }
