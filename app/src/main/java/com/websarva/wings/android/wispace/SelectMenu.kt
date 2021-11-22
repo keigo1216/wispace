84ac91f4025cbc94e9d3e1705c9dc66584ce077b
keigo <ketchup1216@icloud.com>
2021-11-22 14:48:02 +0900

    create select screen

---
+++ app/src/main/java/com/websarva/wings/android/wispace/SelectMenu.kt
@@ -0,0 +1,11 @@
+package com.websarva.wings.android.wispace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_menu)
+    }
+}
