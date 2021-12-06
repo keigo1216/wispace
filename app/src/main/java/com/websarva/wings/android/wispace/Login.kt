2c7b93d5161ee7c6898fdc4ea7f2f24b9f04aeca
keigo <ketchup1216@icloud.com>
2021-12-06 14:11:19 +0900

    12/6日スタート firebaseと連携

--- app/src/main/java/com/websarva/wings/android/wispace/Login.kt
+++ app/src/main/java/com/websarva/wings/android/wispace/Login.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
